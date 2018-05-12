4e15ae0473de6ccb01b632f76ffce1810e8a1ad9
Jebisan H. Nadarajah <Jebisan.h.n@hotmail.com>
2018-05-12 15:14:46 +0200

    God commit message

--- app/src/main/java/alex/karim/jebi/androidmusicdb/Domain/Search/SearchSongTitelTask.java
+++ app/src/main/java/alex/karim/jebi/androidmusicdb/Domain/Search/SearchSongTitelTask.java
@@ -1,15 +1,24 @@
 package alex.karim.jebi.androidmusicdb.Domain.Search;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
 
 import java.util.ArrayList;
+
+import static android.content.ContentValues.TAG;
 
 public class SearchSongTitelTask extends AsyncTask<String, Void, ArrayList<Song>> {
 
 
     @Override
     protected ArrayList<Song> doInBackground(String... strings) {
+        HttpHandler sh = new HttpHandler();
+        // Making a request to url and getting response
+        String url = "http://ws.audioscrobbler.com/2.0/?method=track.search&track=hot&api_key=e3bab7f8adef7e0490d767e0305dd7ce&format=json";
+        String jsonStr = sh.makeServiceCall(url);
+
+        Log.e(TAG, "Response from url: " + jsonStr);
 
 
         return null;
