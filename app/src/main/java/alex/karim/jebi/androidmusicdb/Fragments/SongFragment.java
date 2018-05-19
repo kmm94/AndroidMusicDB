ef68ca686ab3bc88e704aa586afab41f0a1d76ee
karim møller <karimkuku@gmail.com>
2018-05-19 12:30:52 +0200

    add system out printl

--- app/src/main/java/alex/karim/jebi/androidmusicdb/Fragments/SongFragment.java
+++ app/src/main/java/alex/karim/jebi/androidmusicdb/Fragments/SongFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -83,6 +84,7 @@ public class SongFragment extends Fragment implements IUpdateContent {
     @Override
     public void updateAdapterContent() {
         if (songsRecycleView != null) {
+            Log.i("UpdatingViewList", "songs");
             songsRecycleView.getAdapter().notifyDataSetChanged();
         }
     }
