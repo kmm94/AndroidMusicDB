d622e905eefab483aa756b81ea370f694932522d
karim møller <karimkuku@gmail.com>
2018-05-19 12:30:06 +0200

    add system out printl

--- app/src/main/java/alex/karim/jebi/androidmusicdb/Fragments/AlbumFragment.java
+++ app/src/main/java/alex/karim/jebi/androidmusicdb/Fragments/AlbumFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -86,6 +87,7 @@ public class AlbumFragment extends Fragment implements IUpdateContent {
     @Override
     public void updateAdapterContent() {
         if (albumRecycleView != null) {
+            Log.i("UpdatingViewList", "album");
             albumRecycleView.getAdapter().notifyDataSetChanged();
         }
     }
